f68e70531ea0803c1c361def37386b6ebb796e25
Jackson Lima <adrigato3@gmail.com>
2022-10-14 11:05:00 -0500

    [IMP] Childrends and parents routes are already created. Apply lazyload to get childrens routes

---
+++ src/app/heroes/pages/hereo/hereo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hereo',
+  templateUrl: './hereo.component.html',
+})
+export class HereoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
